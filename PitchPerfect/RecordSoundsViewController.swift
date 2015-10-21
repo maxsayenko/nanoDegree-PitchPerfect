fcc5b16b0b2b513c9c181ce3ff9497c72f25eb70
Max Saienko <msaienko@ancestry.com>
2015-10-20 23:40:47 -0700

    initial audio implementation

--- PitchPerfect/RecordSoundsViewController.swift
+++ PitchPerfect/RecordSoundsViewController.swift
@@ -7,8 +7,13 @@
 //
 
 import UIKit
+import AVFoundation
 
 class RecordSoundsViewController: UIViewController {
+    var recorder: AVAudioRecorder!
+    var player:AVAudioPlayer!
+    var meterTimer:NSTimer!
+    var soundFileURL:NSURL?
     
     var isRecording: Bool = false
     let textRecording = "Recording!"
@@ -22,6 +27,7 @@ class RecordSoundsViewController: UIViewController {
         if(isRecording) {
             recordLabel.text = textResume
         } else {
+            self.recordWithPermission(setup: true)
             recordLabel.text = textRecording
             stopButton.hidden = false
         }
@@ -29,8 +35,15 @@ class RecordSoundsViewController: UIViewController {
     }
     
     @IBAction func stopButtonTouch(sender: UIButton) {
-        let playSoundsViewController = self.storyboard?.instantiateViewControllerWithIdentifier("playSoundsView") as! PlaySoundsViewController
-        self.navigationController?.pushViewController(playSoundsViewController, animated: true)
+        //let playSoundsViewController = self.storyboard?.instantiateViewControllerWithIdentifier("playSoundsView") as! PlaySoundsViewController
+        //self.navigationController?.pushViewController(playSoundsViewController, animated: true)
+        
+        print("stop")
+        
+        recorder?.stop()
+        player?.stop()
+        
+        play()
         
     }
     
@@ -50,5 +63,114 @@ class RecordSoundsViewController: UIViewController {
     }
     
     
+    
+    func play() {
+        
+        var url:NSURL?
+        if self.recorder != nil {
+            url = self.recorder.url
+        } else {
+            url = self.soundFileURL!
+        }
+        print("playing \(url)")
+        
+        do {
+            self.player = try AVAudioPlayer(contentsOfURL: url!)
+            player.delegate = self
+            player.prepareToPlay()
+            player.volume = 1.0
+            player.play()
+        } catch let error as NSError {
+            self.player = nil
+            print(error.localizedDescription)
+        }
+        
+    }
+    
+    func setupRecorder() {
+        let currentFileName = "pitchPerfectAudioRecord.m4a"
+        
+        let documentsDirectory = NSFileManager.defaultManager().URLsForDirectory(.DocumentDirectory, inDomains: .UserDomainMask)[0]
+        let soundFileURL = documentsDirectory.URLByAppendingPathComponent(currentFileName)
+        
+        let recordSettings:[String : AnyObject] = [
+            AVFormatIDKey: NSNumber(unsignedInt:kAudioFormatAppleLossless),
+            AVEncoderAudioQualityKey : AVAudioQuality.Max.rawValue,
+            AVEncoderBitRateKey : 320000,
+            AVNumberOfChannelsKey: 2,
+            AVSampleRateKey : 44100.0
+        ]
+        
+        do {
+            recorder = try AVAudioRecorder(URL: soundFileURL, settings: recordSettings)
+            recorder.delegate = self
+            recorder.meteringEnabled = true
+            recorder.prepareToRecord() // creates/overwrites the file at soundFileURL
+        } catch let error as NSError {
+            recorder = nil
+            print(error.localizedDescription)
+        }
+        
+    }
+    
+    func recordWithPermission(setup setup:Bool) {
+        let session:AVAudioSession = AVAudioSession.sharedInstance()
+        if (session.respondsToSelector("requestRecordPermission:")) {
+            AVAudioSession.sharedInstance().requestRecordPermission({(granted: Bool)-> Void in
+                if granted {
+                    print("Permission to record granted")
+                    self.setSessionPlayAndRecord()
+                    if setup {
+                        self.setupRecorder()
+                    }
+                    self.recorder.record()
+                } else {
+                    print("Permission to record not granted")
+                }
+            })
+        } else {
+            print("requestRecordPermission unrecognized")
+        }
+    }
+    
+    func setSessionPlayAndRecord() {
+        let session = AVAudioSession.sharedInstance()
+        do {
+            try session.setCategory(AVAudioSessionCategoryPlayAndRecord)
+        } catch let error as NSError {
+            print("could not set session category")
+            print(error.localizedDescription)
+        }
+        do {
+            try session.setActive(true)
+        } catch let error as NSError {
+            print("could not make session active")
+            print(error.localizedDescription)
+        }
+    }
+}
+
+extension RecordSoundsViewController : AVAudioRecorderDelegate, AVAudioPlayerDelegate {
+    func audioRecorderDidFinishRecording(recorder: AVAudioRecorder,
+        successfully flag: Bool) {
+            print("finished recording \(flag)")
+    }
+    
+    func audioRecorderEncodeErrorDidOccur(recorder: AVAudioRecorder,
+        error: NSError?) {
+            if let e = error {
+                print("\(e.localizedDescription)")
+            }
+    }
+    
+    func audioPlayerDidFinishPlaying(player: AVAudioPlayer, successfully flag: Bool) {
+        print("finished playing \(flag)")
+    }
+    
+    func audioPlayerDecodeErrorDidOccur(player: AVAudioPlayer, error: NSError?) {
+        if let e = error {
+            print("\(e.localizedDescription)")
+        }
+    }
 }
 
