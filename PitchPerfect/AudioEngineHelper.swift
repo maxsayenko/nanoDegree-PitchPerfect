cc079141b4d8b1fd589cfd5b61be8782af962541
Max Saienko <msaienko@ancestry.com>
2015-10-29 00:53:30 -0700

    adding echo function

--- PitchPerfect/AudioEngineHelper.swift
+++ PitchPerfect/AudioEngineHelper.swift
@@ -45,17 +45,44 @@ class AudioEngineHelper: NSObject, AVAudioPlayerDelegate {
         commonAudioFunction(-1000, typeOfChange: "pitch")
     }
     
+    func playEcho() {
+        let echoNode = AVAudioUnitDelay()
+        echoNode.delayTime = NSTimeInterval(0.3)
+        
+        let audioPlayerNode = AVAudioPlayerNode()
+        audioPlayerNode.stop()
+        self.stop()
+        
+        audioEngine.attachNode(audioPlayerNode)
+        
+        // Attach the audio effect node corresponding to the user selected effect
+        audioEngine.attachNode(echoNode)
+        
+        // Connect Player --> AudioEffect
+        audioEngine.connect(audioPlayerNode, to: echoNode, format: audioFile.processingFormat)
+        // Connect AudioEffect --> Output
+        audioEngine.connect(echoNode, to: audioEngine.outputNode, format: audioFile.processingFormat)
+        
+        //audioPlayerNode.scheduleFile(audioFile, atTime: nil, completionHandler:nil)
+        
+       // audioEngine.startAndReturnError(nil)
+        
+        //audioPlayerNode.play()
+        
+        outputAudio(audioPlayerNode)
+    }
+    
     func stop() {
         audioEngine.stop()
         audioEngine.reset()
     }
     
-    func commonAudioFunction(audioChangeNumber: Float, typeOfChange: String){
+    private func commonAudioFunction(audioChangeNumber: Float, typeOfChange: String) {
         let audioPlayerNode = AVAudioPlayerNode()
 
+        // Stopping all current processes
         audioPlayerNode.stop()
-        audioEngine.stop()
-        audioEngine.reset()
+        self.stop()
         
         audioEngine.attachNode(audioPlayerNode)
         
@@ -71,6 +98,34 @@ class AudioEngineHelper: NSObject, AVAudioPlayerDelegate {
         audioEngine.connect(audioPlayerNode, to: changeAudioUnitTime, format: nil)
         audioEngine.connect(changeAudioUnitTime, to: audioEngine.outputNode, format: nil)
         
+        outputAudio(audioPlayerNode)
+        
+//        // Scheduling file to play. Had to convert it to buffer as in 'scheduleFile' function completionHandler didn't fire correctly
+//        do {
+//            // Converting file to buffer
+//            let file: AVAudioFile = try AVAudioFile(forReading: audioFile.url)
+//            let buffer = AVAudioPCMBuffer(PCMFormat: file.processingFormat, frameCapacity: UInt32(file.length))
+//            try file.readIntoBuffer(buffer)
+//            //  Scheduling buffer
+//            audioPlayerNode.scheduleBuffer(buffer, completionHandler: finishPlayingCallback)
+//        }
+//        catch let error {
+//            print("Playing Error")
+//            print(error)
+//        }
+//        
+//        do {
+//           try audioEngine.start()
+//        }
+//        catch let error {
+//            print("Audio Engine StartError")
+//            print(error)
+//        }
+//        
+//        audioPlayerNode.play()
+    }
+    
+    private func outputAudio(playerNode: AVAudioPlayerNode) {
         // Scheduling file to play. Had to convert it to buffer as in 'scheduleFile' function completionHandler didn't fire correctly
         do {
             // Converting file to buffer
@@ -78,7 +133,7 @@ class AudioEngineHelper: NSObject, AVAudioPlayerDelegate {
             let buffer = AVAudioPCMBuffer(PCMFormat: file.processingFormat, frameCapacity: UInt32(file.length))
             try file.readIntoBuffer(buffer)
             //  Scheduling buffer
-            audioPlayerNode.scheduleBuffer(buffer, completionHandler: finishPlayingCallback)
+            playerNode.scheduleBuffer(buffer, completionHandler: finishPlayingCallback)
         }
         catch let error {
             print("Playing Error")
@@ -86,13 +141,13 @@ class AudioEngineHelper: NSObject, AVAudioPlayerDelegate {
         }
         
         do {
-           try audioEngine.start()
+            try audioEngine.start()
         }
         catch let error {
             print("Audio Engine StartError")
             print(error)
         }
         
-        audioPlayerNode.play()
+        playerNode.play()
     }
 }
